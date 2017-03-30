60bf178eb6e2bae9859eca0cad5d581ddc7dd8db
canfeit <canfeit@qq.com>
2017-03-30 15:57:02 +0800

    u

--- js/EditorUi.js
+++ js/EditorUi.js
@@ -952,7 +952,7 @@ EditorUi.prototype.toolbarHeight = 34;
 /**
  * Specifies the height of the footer. Default is 28.
  */
-EditorUi.prototype.footerHeight = 10;
+EditorUi.prototype.footerHeight = 28;
 
 /**
  * Specifies the height of the optional sidebarFooterContainer. Default is 34.
