cd462e36a4304066bc40bcf02be086954801beb2
灿飞 <canfeit@users.noreply.github.com>
2017-04-21 11:25:50 +0800

    Update EditorUi.js

--- js/EditorUi.js
+++ js/EditorUi.js
@@ -3667,7 +3667,6 @@ EditorUi.prototype.save = function(name)
 
 EditorUi.prototype.getModelJsonString = function()
 {
-	var tocollapse = this.actions.get('collapse').isEnabled()
 	var graph = this.editor.graph;
 	graph.selectAll(null, true);
     if (!graph.isSelectionEmpty()) {
@@ -3741,6 +3740,7 @@ EditorUi.prototype.getModelJsonString = function()
 		else {
         	// groupFlag = true;
         	group = graph.getSelectionCell();
+			graph.foldCells(false)//展开组
 		}
 
         var doc = mxUtils.createXmlDocument();
@@ -3752,8 +3752,7 @@ EditorUi.prototype.getModelJsonString = function()
         group.setValue(obj);
 
 		//update by wang,jianhui--start
-		if (!tocollapse) this.actions.get('collapsible').funct()
-		else graph.foldCells(false)//展开
+		if(!this.actions.get('collapse').isEnabled()) this.actions.get('collapsible').funct()
 		var xml = this.editor.getGraphXml(this);
 		var bounds = xml.getElementsByTagName("mxGeometry")[0];
 		bounds.setAttribute("x", 0);
